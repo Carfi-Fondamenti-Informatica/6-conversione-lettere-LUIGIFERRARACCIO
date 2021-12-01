87da5660a41bb03b74e4a39d902cf95a065fe18a
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-01 11:41:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,10 +1,12 @@
 #include "lib.h"
 bool conversione (char &a) {
   bool t=true;
-  if (a>65 and a<=90){
-    a=a+32;
-    return a;
-  } else {
+  if (a>=65 and a<=90 or a>=97 and a<=122){
+    t=true;
+    if (a>=65 and a<=90){
+      a=a+32;
+      return a;
+  }else{
     a=a-32;
     return a;
   }
@@ -13,5 +15,3 @@ bool conversione (char &a) {
 }
 return t;
 }
-return t;
-} 
