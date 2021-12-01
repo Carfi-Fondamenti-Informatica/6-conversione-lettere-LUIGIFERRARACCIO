d01b3349bf7d876467c1284255885159164c4c51
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-01 11:32:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool conversione (char &a) {
+  bool t=true;
+  if (a>65 and a<=90){
+    a=a+32;
+    return a;
+  } else {
+    a=a-32;
+    return a;
+  }
+}else{
+  t=false;
+}
+return t;
+}
+return t;
+} 
